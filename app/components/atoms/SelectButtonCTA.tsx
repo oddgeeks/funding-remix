8ac86652978f35aac60c48f510d62f14adbff486
oddgeeks <oddgeeks.dev@gmail.com>
2023-06-07 21:48:40 +0300

    select button cta

---
+++ app/components/atoms/SelectButtonCTA.tsx
@@ -0,0 +1,11 @@
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+
+export const SelectButtonCTA = () => (
+  <a
+    href="https://myfundedfx.tech/account/register/"
+    className="inline-block px-2 py-1 rounded-md shadow-md bg-funded-50 hover:bg-funded-70"
+  >
+    <CheckCircleIcon className="w-6 h-6 mx-auto" />
+    Challenge
+  </a>
+);
