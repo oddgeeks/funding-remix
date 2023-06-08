9613b052d6232cc13a73933718fa2ef4d58cb6bc
oddgeeks <oddgeeks.dev@gmail.com>
2023-06-08 07:18:29 +0300

    mobile table data

---
+++ app/data/mobileTableData.ts
@@ -0,0 +1,437 @@
+export const twoStep = [
+  {
+    id: 20,
+    heading: "$5K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1", "Phase 2"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["8%", "5%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days", "1 days"],
+      },
+      {
+        title: "Trading Period",
+        columns: ["Unlimited", "Unlimited"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["5%", "5%"],
+      },
+      {
+        title: "Max Initial Account Balance Loss",
+        columns: ["8%", "8%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1", "100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$50", "$50"],
+      },
+    ],
+  },
+  {
+    id: 40,
+    heading: "$10K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1", "Phase 2"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["8%", "5%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days", "1 days"],
+      },
+      {
+        title: "Trading Period",
+        columns: ["Unlimited", "Unlimited"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["5%", "5%"],
+      },
+      {
+        title: "Max Initial Account Balance Loss",
+        columns: ["8%", "8%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1", "100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$100", "$100"],
+      },
+    ],
+  },
+  {
+    id: 60,
+    heading: "$25K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1", "Phase 2"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["8%", "5%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days", "1 days"],
+      },
+      {
+        title: "Trading Period",
+        columns: ["Unlimited", "Unlimited"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["5%", "5%"],
+      },
+      {
+        title: "Max Initial Account Balance Loss",
+        columns: ["8%", "8%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1", "100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$200", "$200"],
+      },
+    ],
+  },
+  {
+    id: 80,
+    heading: "$50K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1", "Phase 2"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["8%", "5%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days", "1 days"],
+      },
+      {
+        title: "Trading Period",
+        columns: ["Unlimited", "Unlimited"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["5%", "5%"],
+      },
+      {
+        title: "Max Initial Account Balance Loss",
+        columns: ["8%", "8%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1", "100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$300", "$300"],
+      },
+    ],
+  },
+  {
+    id: 100,
+    heading: "$100K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1", "Phase 2"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["8%", "5%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days", "1 days"],
+      },
+      {
+        title: "Trading Period",
+        columns: ["Unlimited", "Unlimited"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["5%", "5%"],
+      },
+      {
+        title: "Max Initial Account Balance Loss",
+        columns: ["8%", "8%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1", "100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$500", "$500"],
+      },
+    ],
+  },
+  {
+    id: 120,
+    heading: "$200K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1", "Phase 2"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["8%", "5%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days", "1 days"],
+      },
+      {
+        title: "Trading Period",
+        columns: ["Unlimited", "Unlimited"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["5%", "5%"],
+      },
+      {
+        title: "Max Initial Account Balance Loss",
+        columns: ["8%", "8%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1", "100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$950", "$950"],
+      },
+    ],
+  },
+];
+
+export const oneStep = [
+  {
+    id: 20,
+    heading: "$5K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["10%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["4%"],
+      },
+      {
+        title: "Max Trailing Loss",
+        columns: ["6%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$50"],
+      },
+    ],
+  },
+  {
+    id: 40,
+    heading: "$10K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["10%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["4%"],
+      },
+      {
+        title: "Max Trailing Loss",
+        columns: ["6%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$100"],
+      },
+    ],
+  },
+  {
+    id: 60,
+    heading: "$25K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["10%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["4%"],
+      },
+      {
+        title: "Max Trailing Loss",
+        columns: ["6%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$200"],
+      },
+    ],
+  },
+  {
+    id: 80,
+    heading: "$50K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["10%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["4%"],
+      },
+      {
+        title: "Max Trailing Loss",
+        columns: ["6%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$300"],
+      },
+    ],
+  },
+  {
+    id: 100,
+    heading: "$100K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["10%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["4%"],
+      },
+      {
+        title: "Max Trailing Loss",
+        columns: ["6%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$500"],
+      },
+    ],
+  },
+  {
+    id: 120,
+    heading: "$200K",
+    rows: [
+      {
+        title: "",
+        columns: ["Phase 1"],
+      },
+      {
+        title: "Profit Target",
+        columns: ["10%"],
+      },
+      {
+        title: "Minimum Trading Days",
+        columns: ["1 days"],
+      },
+      {
+        title: "Max Daily Loss",
+        columns: ["4%"],
+      },
+      {
+        title: "Max Trailing Loss",
+        columns: ["6%"],
+      },
+      {
+        title: "Available Leverage",
+        columns: ["100:1"],
+      },
+      {
+        title: "Refundable Registration Fee",
+        columns: ["$950"],
+      },
+    ],
+  },
+];
